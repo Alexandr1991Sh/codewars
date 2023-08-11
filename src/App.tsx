dff84b3abfde47c54dcc044442b91f2311ebc3bc
Alexandr1991Sh <an1991sh@yandex.ru>
2023-08-11 08:52:34 +0300

    new commit (test)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //useState
+    let [playerCounter1, playCounter1] = useState(10)
+    let [playerCounter2, playCounter2] = useState(10)
+
+    return (
+        <div className="App">
+            {/*<div>Alex</div>*/}
+            {/*<div>{playerCounter1}</div>*/}
+            {/*<button onClick={() => {*/}
+            {/*    playCounter1((actual)=> actual + 1)*/}
+            {/*}}>+*/}
+            {/*</button>*/}
+            {/*<hr/>*/}
+            {/*<div>Dima</div>*/}
+            {/*<div>{playerCounter2}</div>*/}
+            {/*<button onClick={() => {*/}
+            {/*    playCounter2((actual)=> actual + 1)*/}
+            {/*}}>+*/}
+            {/*</button>*/}
+            {/*<hr/>*/}
+            {/*<button onClick={() => {*/}
+            {/*    playCounter1((actual)=> actual - 1)*/}
+            {/*    playCounter2((actual)=> actual - 1)*/}
+            {/*}}>-*/}
+            {/*</button>*/}
+        </div>
+    );
 }
 
 export default App;
+
+
+
